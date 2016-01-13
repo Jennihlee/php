a6fa5f97c0dd3525ec26a3cff5a9cecb445b4181
Ji Hyeon <ljihyun0126@naver.com>
2016-01-13 13:56:48 +0900

    #4 CSS 코드 추가

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
+<style>
+  b { 
+       /* c스타일의 주석 */
+       color:red; 
+       font-weight:normal;
+       background-color:yellow; 
+  }
+
+  #hellobox{
+       width:500px; height:300px;
+       border:1px solid #ff0000;
+       margin:10px;
+       padding:10px;
+       position:absolute; top:20px; right:26px;
+  }
+  .nicename{
+       font-weight:bold;
+       font-size:2px;
+       text-decoration:underline;
+       cursor:pointer;
+  }
+</style>
+
+<div id="hellobox">
+      Have a nice day!
+</div>
 <?php
 
-for($i=0;$i<10000;i++){
-  echo $i . " Hello, Ji Hyeon!<br >" ;
+for($i=0;$i<10000;$i++){
+  echo $i . " <b>Hello</b>, <span class='nicename'>Ji Hyeon!</span><br >" ;
 }
 
 ?>
