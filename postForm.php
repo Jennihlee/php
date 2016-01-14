f19f82a91b2fa996694c1dbe2373d4b6dee07050
Ji Hyeon <ljihyun0126@naver.com>
2016-01-14 15:34:37 +0900

    #5 POST 연습

---
+++ postForm.php
@@ -0,0 +1,10 @@
+<form method="post" action="postResult.php">
+<h1>For you, VOMVOM service~</h1>
+Input Your Name<br >
+<input type="text" name="name" id="name" value=""><br>
+Input Your Birthday<br>
+<input type="text" name="year" id="year" value="">
+<input type="text" name="month" id="month" value="">
+<input type="text" name="day" id="day" value=""><br>
+<input type="submit" value="View Your Resurt">
+</form>
---
+++ postResult.php
@@ -0,0 +1,12 @@
+<?php
+$name=$_POST['name'];
+$year=$_POST['year'];
+$month=$_POST['month'];
+$day=$_POST['day'];
+$age=2016-$year+1;
+?>
+
+<h1>WOW!</h1>
+I know you, <?=$name?>!<br>
+Your birthday is <?=$year?>-<?=$month?>-<?=$day?><br>.
+Your age : <?=$age?>
