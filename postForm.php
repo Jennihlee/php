5881c859165624430256b7f25d1f5d0358d32ea1
Ji Hyeon <ljihyun0126@naver.com>
2016-01-15 13:48:13 +0900

    #6 포스트 작동 연습(로그인)

--- postForm.php
+++ postForm.php
@@ -1,10 +1,41 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8"); 
+?>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+<?php
+$isLogin = $_COOKIE['isLogin'];
+if($isLogin !=1){
+?>
+
 <form method="post" action="postResult.php">
-<h1>For you, VOMVOM service~</h1>
-Input Your Name<br >
-<input type="text" name="name" id="name" value=""><br>
-Input Your Birthday<br>
-<input type="text" name="year" id="year" value="">
-<input type="text" name="month" id="month" value="">
-<input type="text" name="day" id="day" value=""><br>
-<input type="submit" value="View Your Resurt">
+
+<form class="navbar-form navbar-left">
+  <div class="form-group">
+
+    <input type="text" id="name" name="name" class="form-control" placeholder="id를 입력하세요.">
+    <input type="password" class="form-control" id="pw" name="pw" placeholder="암호를 입력하세요.">
+
+</div>
+  <button type="submit" class="btn btn-default">로그인 하기</button>
+
 </form>
+<?php
+}else{
+	$name=$_COOKIE['name'];
+?>
+<?=$name?>님, 이미 로그인이 되어 있습니다.<br>
+<a href="postLogout.php" class="btn btn-default">LOGOUT</a>
+<?php 
+} 
+?>
+
--- postResult.php
+++ postResult.php
@@ -1,12 +1,32 @@
+<?php header("Content-Type: text/html; charset=UTF-8"); ?>
+
 <?php
 $name=$_POST['name'];
-$year=$_POST['year'];
-$month=$_POST['month'];
-$day=$_POST['day'];
-$age=2016-$year+1;
+$password=$_POST['pw'];
 ?>
 
-<h1>WOW!</h1>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<?php
+if($name=="id" && $password=="pw"){
+	setCookie('isLogin', '1');
+	setCookie('name', $name);
+?> <h1>로그인 완료</h1>
 I know you, <?=$name?>!<br>
-Your birthday is <?=$year?>-<?=$month?>-<?=$day?><br>.
-Your age : <?=$age?>
+Your password is <?=$password?><br>
+<a href="./postForm.php">BACK</a>
+<?php
+}else{
+	setCookie('isLogin', '0');
+	setCookie('name', '');
+?>
+사용자가 없습니다.<br>
+다시 로그인 해보세요.<br>
+<a href="./postForm.php">BACK</a>
+<?php } ?>
